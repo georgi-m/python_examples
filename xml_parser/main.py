1ae21fccb181f6fd293ac3e060c1b29d2503d7fe
Georgi Mirzoyan <mirzoyan87@mail.ru>
2017-05-29 23:19:56 +0400

    Updated util and main files.

--- xml_parser/main.py
+++ xml_parser/main.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python 
 
-import argparse
-
-### Parse command-line arguments.
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', action='store', dest='ini_file',
-                help='Set path to the ini file.', required=True)
-results = parser.parse_args()
+import util
--- xml_parser/util.py
+++ xml_parser/util.py
@@ -1,7 +1,15 @@
 import argparse
+import logging
+import logging.config
 
 ### Parse command-line arguments.
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', action='store', dest='ini_file',
                 help='Set path to the ini file.', required=True)
 results = parser.parse_args()
+
+
+logging.config.fileConfig(results.ini_file)
+logger = logging.getLogger()
+logger.debug('often makes a very good meal of %s', 'visiting tourists')
+logger.info('info loggin is working %s', 'visiting tourists')
