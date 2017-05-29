ad26fd385958849b11baa17b91ef7953fe68c9ca
Georgi Mirzoyan <mirzoyan87@mail.ru>
2017-05-29 21:33:56 +0400

    Added command-line parser.
    Author: Georgi Mirzoyan

--- xml_parser/main.py
+++ xml_parser/main.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python 
+
 import argparse
 
 ### Parse command-line arguments.
 parser = argparse.ArgumentParser()
-parser.add_argument('-l', action='store', dest='log_level',
-                help='Setting log level.')
+parser.add_argument('-i', action='store', dest='ini_file',
+                help='Set path to the ini file.', required=True)
+results = parser.parse_args()
