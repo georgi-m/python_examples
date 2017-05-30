0fdceaeab5ed029765a11f53ae4c443275c33779
Georgi Mirzoyan <mirzoyan87@mail.ru>
2017-05-30 12:32:38 +0400

    Added parse logic.
    Author: Georgi Mirzoyan

--- xml_parser/main.py
+++ xml_parser/main.py
@@ -1,3 +1,38 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
 import util
+import xml.etree.ElementTree as ET
+import os
+
+### Getting xml files. 
+def get_xml_files(d):
+    xmls = []
+    for file in os.listdir(d):
+        if file.endswith(".xml"):
+            xmls.append(os.path.join(d, file))
+            util.logger.debug(os.path.join(d, file))
+    return xmls
+
+### Parse xmls and get min value
+def get_min_element_from_xmls(xmls_list):
+    numbers = []
+    for path in xmls_list:
+        util.logger.debug('path to xml %s', path)
+        tree = ET.parse(path)
+        root = tree.getroot()
+
+        for human in root.findall('Lusine'):
+            m = int(human.find('min').text)
+            numbers.append(m)
+
+    a = min(numbers, key=int)
+    util.logger.debug('minimum from all xmls is %i', a)
+    util.logger.debug('xml file %s name %s min value %s',path, human, m)
+
+### Main function.
+def main():
+    xmls_list = get_xml_files("./input_xml_dir")
+    m = get_min_element_from_xmls(xmls_list)
+#    push_result()
+
+main()
--- xml_parser/util.py
+++ xml_parser/util.py
@@ -8,8 +8,8 @@ parser.add_argument('-i', action='store', dest='ini_file',
                 help='Set path to the ini file.', required=True)
 results = parser.parse_args()
 
-
 logging.config.fileConfig(results.ini_file)
 logger = logging.getLogger()
-logger.debug('often makes a very good meal of %s', 'visiting tourists')
-logger.info('info loggin is working %s', 'visiting tourists')
+#logger.debug('often makes a very good meal of %s', 'visiting tourists')
+#logger.info('info loggin is working %s', 'visiting tourists')
+
